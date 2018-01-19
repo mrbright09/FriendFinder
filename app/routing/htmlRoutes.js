1759a7b0d1eba30eb6bd93409326c7ccb598c379
Mikal Bright <MikalBright@gmail.com>
2018-01-18 20:24:43 -0500

    Updated path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,9 @@
 var path = require('path');
 module.exports = function(app){
-	app.get('/public/survey', function(req, res){
+	app.get('/survey', function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/survey.html'));
 	});
-	app.use(function(req, res){
+	app.get('/', function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/home.html'));
 	});
 }
