771df16e34098a5a86cc40c8d698a322b91d4ec5
Mikal Bright <MikalBright@gmail.com>
2018-01-18 19:10:50 -0500

    Updated file names

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);app.listen(PORT, function() {
+app.use(bodyParser.json({type:'application/vnd.api+json'}));require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
