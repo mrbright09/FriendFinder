17ba8814c07c0f4c611b665c34da38090f01f42e
Mikal Bright <MikalBright@gmail.com>
2018-01-18 19:22:38 -0500

    Updated file names

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,10 +2,10 @@ var friendData 		= require('../data/friends.js');
 var path 			= require('path');
 var totalDifference = 0;
 module.exports = function(app){
-	app.get('/api/friends', function(req, res){
+	app.get('/data/friends', function(req, res){
 		res.json(friends);
 	});
-	app.post('/api/friends', function(req, res){
+	app.post('/data/friends', function(req, res){
 		var greatMatch = {
 			name: "",
 			image: "",
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,6 @@
 var path = require('path');
 module.exports = function(app){
-	app.get('/survey', function(req, res){
+	app.get('/public/survey', function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/survey.html'));
 	});
 	app.use(function(req, res){
